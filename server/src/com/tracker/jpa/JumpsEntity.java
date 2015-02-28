5f6d890e221f6f3094fa46146cf69e1749d35266
Ivan <cyberdd3@gmail.com>
2015-02-28 15:43:08 +0300

    Persistence entities

---
+++ server/src/com/tracker/jpa/JumpsEntity.java
@@ -0,0 +1,75 @@
+package com.tracker.jpa;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "jumps", schema = "xtracker", catalog = "x-tracker")
+public class JumpsEntity {
+    private long jumpId;
+    private Long trackId;
+    private Integer airTime;
+    private TracksEntity tracksByTrackId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "jump_id", nullable = false, insertable = true, updatable = true)
+    public long getJumpId() {
+        return jumpId;
+    }
+
+    public void setJumpId(long jumpId) {
+        this.jumpId = jumpId;
+    }
+
+    @Basic
+    @Column(name = "track_id", nullable = true, insertable = true, updatable = true)
+    public Long getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(Long trackId) {
+        this.trackId = trackId;
+    }
+
+    @Basic
+    @Column(name = "air_time", nullable = true, insertable = true, updatable = true)
+    public Integer getAirTime() {
+        return airTime;
+    }
+
+    public void setAirTime(Integer airTime) {
+        this.airTime = airTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JumpsEntity that = (JumpsEntity) o;
+
+        if (jumpId != that.jumpId) return false;
+        if (airTime != null ? !airTime.equals(that.airTime) : that.airTime != null) return false;
+        if (trackId != null ? !trackId.equals(that.trackId) : that.trackId != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (jumpId ^ (jumpId >>> 32));
+        result = 31 * result + (trackId != null ? trackId.hashCode() : 0);
+        result = 31 * result + (airTime != null ? airTime.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "track_id", referencedColumnName = "track_id")
+    public TracksEntity getTracksByTrackId() {
+        return tracksByTrackId;
+    }
+
+    public void setTracksByTrackId(TracksEntity tracksByTrackId) {
+        this.tracksByTrackId = tracksByTrackId;
+    }
+}
---
+++ server/src/com/tracker/jpa/UsersEntity.java
@@ -0,0 +1,75 @@
+package com.tracker.jpa;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users", schema = "xtracker", catalog = "x-tracker")
+public class UsersEntity {
+    private long userId;
+    private String token;
+    private String email;
+    private Collection<TracksEntity> tracksesByUserId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false, insertable = true, updatable = true)
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "token", nullable = true, insertable = true, updatable = true, length = 2147483647)
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, insertable = true, updatable = true, length = 2147483647)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (userId != that.userId) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (token != null ? !token.equals(that.token) : that.token != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (userId ^ (userId >>> 32));
+        result = 31 * result + (token != null ? token.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+
+    @OneToMany(mappedBy = "usersByUserId")
+    public Collection<TracksEntity> getTracksesByUserId() {
+        return tracksesByUserId;
+    }
+
+    public void setTracksesByUserId(Collection<TracksEntity> tracksesByUserId) {
+        this.tracksesByUserId = tracksesByUserId;
+    }
+}
