855e323f89c22cf7f975abbd12acf1277749a244
Vladimir Ivashkin <illusionww@gmail.com>
2015-03-04 03:21:08 +0300

    jsf works, jpa not

--- server/src/com/xtracker/ejb/SessionBean.java
+++ server/src/com/xtracker/ejb/SessionBean.java
@@ -1,9 +1,19 @@
 package com.xtracker.ejb;
 
+import com.xtracker.jpa.UsersEntity;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless(name = "ExampleBeanEJB")
 public class SessionBean {
+
+    @PersistenceContext
+    EntityManager em;
+
     public SessionBean() {
+        UsersEntity usersEntity = new UsersEntity();
+        em.persist(usersEntity);
     }
 }
