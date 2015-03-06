747f7ff9fabbb588b4f768cbeac2bcfda36bd4dc
VI-yvi-x <ilya.vasilev@phystech.edu>
2015-03-06 16:42:51 +0300

    Small additions in Tracking

--- client/X-tracker/app/src/main/java/com/ncedu/x_tracker/fragments/ScreenOne.java
+++ client/X-tracker/app/src/main/java/com/ncedu/x_tracker/fragments/ScreenOne.java
@@ -16,21 +16,38 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.ncedu.x_tracker.MainActivity;
 import com.ncedu.x_tracker.R;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class ScreenOne extends Fragment implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+import static com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import static com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+
+public class ScreenOne extends Fragment implements
+        View.OnClickListener,
+        ConnectionCallbacks,
+        OnConnectionFailedListener,
+        LocationListener {
 
 
     private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private View rootView = null;
+    ImageButton button = null;
+    private boolean mRequestingLocationUpdates;
+    private List<Location> track;
 
     public ScreenOne() {
     }
-
-    View rootView = null;
-    ImageButton button = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,6 +56,10 @@ public class ScreenOne extends Fragment implements View.OnClickListener, GoogleA
         rootView = inflater.inflate(R.layout.screen_one, container,
                 false);
 
+        //Establish track as an array of Locations
+        track = new ArrayList<>();
+
+        //Set ImageButton clickable
         button = (ImageButton) rootView.findViewById(R.id.startCaptureBtn);
         button.setOnClickListener(this);
 
@@ -49,14 +70,25 @@ public class ScreenOne extends Fragment implements View.OnClickListener, GoogleA
                 .addOnConnectionFailedListener(this)
                 .build();
 
+        //Create a LocationRequest
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(3000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
         return rootView;
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.startCaptureBtn) {
+            mRequestingLocationUpdates = true;
             mGoogleApiClient.connect();
             TextView textView1 = (TextView) rootView.findViewById(R.id.textView1);
+
+//            When we will send DATA to server, setFasterInterval should be slower!!!
+
+
         }
     }
 
@@ -64,13 +96,26 @@ public class ScreenOne extends Fragment implements View.OnClickListener, GoogleA
     @Override
     public void onConnected(Bundle bundle) {
         String mLatitude = null, mLongitude = null;
-        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null){
-            mLatitude = String.valueOf(mLastLocation.getLatitude());
-            mLongitude = String.valueOf(mLastLocation.getLongitude());
+        this.mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (this.mCurrentLocation != null) {
+            mLatitude = String.valueOf(mCurrentLocation.getLatitude());
+            mLongitude = String.valueOf(mCurrentLocation.getLongitude());
+            track.add(mCurrentLocation);
             TextView textView1 = (TextView) rootView.findViewById(R.id.textView1);
-            textView1.setText(mLatitude + " | " + mLongitude );
+            textView1.setText(mLatitude + " | " + mLongitude);
         }
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    public void onSavedInstanceState(Bundle savedInstanceState) {
+//        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, mRequestingLocationUpdates);
+
     }
 
     @Override
@@ -82,4 +127,14 @@ public class ScreenOne extends Fragment implements View.OnClickListener, GoogleA
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+    }
+
+    private void updateUI() {
+
+    }
+
 }
